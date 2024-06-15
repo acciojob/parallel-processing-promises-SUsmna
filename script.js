b1b42040e72ab18a00e9185e0f9ab7a352e0a4e7
SUsmna <159513831+SUsmna@users.noreply.github.com>
2024-06-15 20:23:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,36 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+  const outputDiv = document.getElementById('output');
+    const downloadButton = document.getElementById('download-images-button');
+
+    downloadButton.addEventListener('click', () => {
+       
+        outputDiv.innerHTML = 'Loading...';
+
+        const imagePromises = imageUrls.map(imgObj => loadImage(imgObj.url));
+
+        Promise.all(imagePromises)
+            .then(images => {
+                
+                outputDiv.innerHTML = ''; 
+                images.forEach(img => {
+                    outputDiv.appendChild(img);
+                });
+            })
+            .catch(error => {
+              
+                outputDiv.innerHTML = error;
+            });
+    });
+
+    function loadImage(url) {
+        return new Promise((resolve, reject) => {
+            const img = new Image();
+            img.src = url;
+            img.onload = () => resolve(img);
+            img.onerror = () => reject(Failed to load image's URL: ${url});
+        });
+    }
+});
